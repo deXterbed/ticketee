a1083ec717c4eac38866030b4828212df1094740
Manoj Mishra <b0rn2c0d3@gmail.com>
2012-10-16 23:00:05 +0530

    Added updating projects functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,4 +22,20 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(params[:project])
+      redirect_to @project, :notice => "Project has been updated."
+    else
+      flash[:alert] = "Project has not been updated."
+      render "edit"
+    end
+
+  end
+
 end
