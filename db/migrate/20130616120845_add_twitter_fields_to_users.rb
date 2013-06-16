4ea9c1518b0ab477df79d2739e56051f51a82299
neifelheim <b0rn2c0d3@gmail.com>
2013-06-17 02:44:22 +0530

    Fixed the field type for twitter_id in users table

--- db/migrate/20130616120845_add_twitter_fields_to_users.rb
+++ db/migrate/20130616120845_add_twitter_fields_to_users.rb
@@ -1,6 +1,6 @@
 class AddTwitterFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :twitter_id, :string
+    add_column :users, :twitter_id, :integer
     add_column :users, :twitter_screen_name, :string
     add_column :users, :twitter_display_name, :string
   end
